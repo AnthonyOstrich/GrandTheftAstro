f65752f5ad3779027ba9a182c2a8fb504c94de93
set <set>
2015-12-19 09:36:55 -0500

    Initial commit

---
+++ desktop/src/com/anthonyostrich/gta/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.anthonyostrich.gta.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.anthonyostrich.gta.GrandTheftAstro;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GrandTheftAstro(), config);
+	}
+}
