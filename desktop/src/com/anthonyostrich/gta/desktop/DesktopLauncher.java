7be19a98e85d99d6d72f357150a5d4caba8c2388
clarkan100@gmail.com <Whatwhat1138>
2016-01-06 19:48:29 -0500

    Updated to Libgdx 1.8 and lwgl3

--- desktop/src/com/anthonyostrich/gta/desktop/DesktopLauncher.java
+++ desktop/src/com/anthonyostrich/gta/desktop/DesktopLauncher.java
@@ -1,14 +1,14 @@
 package com.anthonyostrich.gta.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.anthonyostrich.gta.GrandTheftAstro;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 import java.util.Scanner;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GrandTheftAstro(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new GrandTheftAstro(), config);
 	}
 }
